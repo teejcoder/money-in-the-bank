041966e64803dfb9f751d931660590d287ddd4b6
Teej <96551689+teejcoder@users.noreply.github.com>
2023-07-09 14:45:23 +1000

    Latest Changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 function Header () {
-    const [userName, setUserName] =  useState({
-
-    })
-
+  const [user, setUser] = useState({});
+  
     useEffect(() => {
-        fetch("/profile").then(
-          response => response.json()
-        ).then(
-          data => {
-            setUserName(data)
-            console.log(data)
-          }
-        )
+      fetch("/profile").then(
+        response => response.json()
+      ).then(
+        data => {
+          setUser(data)
+          console.log(data)
+        }
+      )
     }, [])
-
+  
 
     return (
         <>
         <section className="navbar">
-            <h1>hello </h1>
-            <button>LOG OUT</button>
+            <h1>hello, {user.firstName}</h1>
         </section>
         </>
     )
