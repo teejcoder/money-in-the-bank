0a5bd3677817902c75bbe4b0f2e0687969c8f162
Teej <96551689+teejcoder@users.noreply.github.com>
2023-08-08 13:29:12 +1000

    latest changes

--- client/src/components/AuthToken.jsx
+++ client/src/components/AuthToken.jsx
@@ -3,28 +3,27 @@ import React, { useEffect, useState } from 'react'
 const AuthToken = () => {
   const [token, setToken] = useState({})
 
-  useEffect(() =>{
-    const url = "http://localhost:3000/api/authToken"
-    
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.log("error", error);
+  useEffect(() => {
+    fetch("/api/authToken").then(
+      response => response.json()
+    ).then(
+      data => {
+        setToken(data)
+        console.log(data)
       }
-    };
-
-  fetchData();
-  
+    )
   }, [])
-  
+
+  const handleClick = () => {
+    
+  }
+
+
   return (
     <div>
-      <button>connect bank</button>
+      <button onClick={handleClick}>connect bank</button>
     </div>
   )
 }
 
-export default AuthToken
+export default AuthToken;
